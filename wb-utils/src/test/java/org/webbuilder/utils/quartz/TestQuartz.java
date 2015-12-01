1d09a1f08fe80710b968de8b61620ca8a5c276d1
zh.sqy <zh.sqy@qq.com>
2015-12-01 10:28:19 +0800

    测试

--- wb-utils/src/test/java/org/webbuilder/utils/quartz/TestQuartz.java
+++ wb-utils/src/test/java/org/webbuilder/utils/quartz/TestQuartz.java
@@ -3,6 +3,10 @@ package org.webbuilder.utils.quartz;
 import org.junit.Test;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.webbuilder.utils.script.engine.DynamicScriptEngine;
+import org.webbuilder.utils.script.engine.DynamicScriptEngineFactory;
+
+import java.util.HashMap;
 
 /**
  * Created by 浩 on 2015-11-03 0003.
@@ -11,26 +15,19 @@ public class TestQuartz {
 
     @Test
     public void testQuartz() throws Exception {
-        //1秒执行一次
-        String cron = "*/1 * * * * ?";
 
-        QuartzBuilder.addJob(new QuartzJob("haha", "g1", cron, new QuartzJob.JobExecute() {
-            public <T> T execute(JobExecutionContext arg0) throws JobExecutionException {
-                return (T) "11";
-            }
-        }));
+       final DynamicScriptEngine engine = DynamicScriptEngineFactory.getEngine("js");
+        engine.compile("test","var i = '我是js脚本';return i");
 
         QuartzBuilder.addJob(new QuartzJob("hehe", "g1", "*/1 * * * * ?", new QuartzJob.JobExecute() {
             public <T> T execute(JobExecutionContext arg0) throws JobExecutionException {
+                //这里执行业务逻辑
+                System.out.println(engine.execute("test",new HashMap<String, Object>()));
                 return (T) "aa";
             }
         }));
 
         Thread.sleep(5000);
         QuartzBuilder.pauseJob("hehe", "g1");//暂停
-        Thread.sleep(5000);
-        QuartzBuilder.resumeJob("hehe", "g1");//唤醒
-        Thread.sleep(5000);
-        QuartzBuilder.deleteJob("hehe", "g1");//删除
     }
 }
