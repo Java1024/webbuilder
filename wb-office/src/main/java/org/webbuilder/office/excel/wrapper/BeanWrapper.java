4b63f10421b193564f5a376564d479b0d7eb8b47
zh.sqy <zh.sqy@qq.com>
2015-12-14 18:57:52 +0800

    优化BeanWrapper

--- wb-office/src/main/java/org/webbuilder/office/excel/wrapper/BeanWrapper.java
+++ wb-office/src/main/java/org/webbuilder/office/excel/wrapper/BeanWrapper.java
@@ -2,6 +2,9 @@ package org.webbuilder.office.excel.wrapper;
 
 import org.webbuilder.utils.base.ClassUtil;
 import org.webbuilder.utils.base.StringUtil;
+import org.webbuilder.utils.common.BeanUtils;
+import org.webbuilder.utils.common.ClassUtils;
+import org.webbuilder.utils.common.bean.JDKAttributeUtil;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -29,45 +32,27 @@ public class BeanWrapper<T> extends AbstractWrapper<T> {
     public void wrapper(T instance, String header, Object value) {
         if (header == null || "".equals(header)) return;
         header = headerMapper(header);
-        setValue(instance, header, value);
-    }
-
-    protected Method getMethodByName(String field) throws Exception {
-        String setterName = "set".concat(StringUtil.toUpperCaseFirstOne(field));
-        Method method1 = methodCache.get(type.getName().concat(setterName));
-        if (method1 == null) {
-            try {
-                Field field1 = type.getDeclaredField(field);
-                method1 = type.getMethod(setterName, field1.getType());
-                methodCache.put(type.getName().concat(setterName), method1);
-            } catch (Exception e) {
-                return null;
-            }
-        }
-        return method1;
-    }
-
-    protected void setValue(T instance, String field, Object value) {
         try {
-            Method method = getMethodByName(field);
-            if (method != null) {
-                Class<?> paramType = method.getParameterTypes()[0];
-                value = changeType(value, paramType);
-                method.invoke(instance, value);
+            Field field = JDKAttributeUtil.getFiledFromCache(type, header);
+            if (field != null) {
+                value = changeType(value, field.getType());
             }
+            BeanUtils.attr(instance, header, value);
         } catch (Exception e) {
-            e.printStackTrace();
         }
     }
+
 
     protected Object changeType(Object value, Class<?> paramType) {
         if (value.getClass() == paramType) return value;
-        if(paramType ==int.class||paramType==Integer.class){
-            value=StringUtil.toInt(value);
-        } if(paramType ==double.class||paramType==Double.class){
-            value=StringUtil.toDouble(value);
-        }if(paramType ==float.class||paramType==Float.class){
-            value=(float)StringUtil.toDouble(value);
+        if (paramType == int.class || paramType == Integer.class) {
+            value = StringUtil.toInt(value);
+        }
+        if (paramType == double.class || paramType == Double.class) {
+            value = StringUtil.toDouble(value);
+        }
+        if (paramType == float.class || paramType == Float.class) {
+            value = (float) StringUtil.toDouble(value);
         }
         return value;
     }
