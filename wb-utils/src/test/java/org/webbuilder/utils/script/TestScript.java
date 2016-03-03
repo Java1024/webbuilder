20faed792e74898f9b2bd068666e32bb9ea12f46
zh.sqy <zh.sqy@qq.com>
2016-03-03 16:25:13 +0800

    新增了动态执行java代码引擎。

--- wb-utils/src/test/java/org/webbuilder/utils/script/TestScript.java
+++ wb-utils/src/test/java/org/webbuilder/utils/script/TestScript.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.webbuilder.utils.script.engine.DynamicScriptEngine;
 import org.webbuilder.utils.script.engine.DynamicScriptEngineFactory;
 import org.webbuilder.utils.script.engine.ExecuteResult;
+import org.webbuilder.utils.script.engine.common.listener.CommonScriptExecuteListener;
+import org.webbuilder.utils.script.engine.listener.ExecuteEvent;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +25,9 @@ public class TestScript {
         DynamicScriptEngine engine = DynamicScriptEngineFactory.getEngine("js");
         engine.init("var Integer = ".concat(Integer.class.getName()).concat(";"));
         engine.compile("test", "return new Integer(1);");
+
         ExecuteResult result = engine.execute("test", new HashMap<String, Object>());
+        System.out.println(result.getResult());
         Assert.assertEquals(result.getResult(), 1);
     }
 
@@ -33,14 +38,40 @@ public class TestScript {
     public void testGrv() throws Exception {
         DynamicScriptEngine engine = DynamicScriptEngineFactory.getEngine("groovy");
         //engine.init("i=1;", "i = 20;");
-        engine.compile("test", "return user;");
-        ExecuteResult result = engine.execute("test", new HashMap<String, Object>() {
-            {
-                put("user", 20);
-            }
-        });
-        Assert.assertEquals(result.getResult(), 20);
+        engine.compile("user", "\n" +
+                "public class User {\n" +
+                "      {\n" +
+                "        System.out.println(1);\n" +
+                "    }\n" +
+                "\n" +
+                "    public String str;\n" +
+                "}\n ");
     }
+
+    @Test
+    public void testJava() throws Exception {
+        DynamicScriptEngine engine = DynamicScriptEngineFactory.getEngine("java");
+        String id = "org.webbuilder.script.Test";
+        String code = "package org.webbuilder.script;\n" +
+                "import org.webbuilder.utils.script.engine.java.Executor;\n" +
+                "import java.util.*;\n" +
+                "public class Test implements Executor{\n" +
+                "public static Test instance = new Test();\n" +
+                "public static Test getInstance(){return instance;};\n" +
+                "    public Object execute(Map<String, Object> var) throws Exception {\n" +
+                "        return  var+\".aaa\";\n" +
+                "    }\n" +
+                "}";
+        //编译动态代码
+        engine.compile(id, code);
+        //执行动态代码
+        ExecuteResult result =  engine.execute(id,new HashMap<String, Object>(){{
+            put("param","嘿嘿黑");
+        }});
+        System.out.println(result.getResult());
+    }
+
+
     /**
      * 测试执行groovy
      */
@@ -48,7 +79,7 @@ public class TestScript {
     public void testGrvClass() throws Exception {
         DynamicScriptEngine engine = DynamicScriptEngineFactory.getEngine("groovy");
         //engine.init("i=1;", "i = 20;");
-        engine.compile("v","public class AuthInsertBefore{\n" +
+        engine.compile("v", "public class AuthInsertBefore{\n" +
                 "    public static final String valid(String param){\n" +
                 "            return param;\n" +
                 "    }\n" +
