e1f8a4504972e0c89ef95aff60a5f0d51cdef5d3
周浩 <zh.sqy@qq.com>
2016-04-06 10:25:36 +0800

    删除无用模块，修改模块名称

--- wb-search/src/main/java/org/webbuilder/search/engine/SearchResult.java
+++
@@ -1,9 +0,0 @@
-package org.webbuilder.search.engine;
-
-import java.util.LinkedHashMap;
-
-/**
- * Created by 浩 on 2015-11-04 0004.
- */
-public class SearchResult extends LinkedHashMap<String, Object> {
-}
