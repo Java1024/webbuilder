b16b0e063e3f486858ebee7362a97e488b00b1d8
周浩 <zh.sqy@qq.com>
2016-04-06 10:32:51 +0800

    删除无用的引入

--- wb-core/src/main/java/org/webbuilder/web/controller/user/UserController.java
+++ wb-core/src/main/java/org/webbuilder/web/controller/user/UserController.java
@@ -1,18 +1,18 @@
 package org.webbuilder.web.controller.user;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.webbuilder.web.core.aop.logger.AccessLogger;
 import org.webbuilder.web.core.authorize.annotation.Authorize;
 import org.webbuilder.web.core.bean.JsonParam;
 import org.webbuilder.web.core.bean.PageUtil;
 import org.webbuilder.web.core.bean.ResponseData;
 import org.webbuilder.web.core.bean.ResponseMessage;
-import org.webbuilder.web.po.role.Role;
+import org.webbuilder.web.core.controller.GenericController;
 import org.webbuilder.web.po.user.User;
 import org.webbuilder.web.service.user.UserService;
-import org.webbuilder.web.core.controller.GenericController;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
