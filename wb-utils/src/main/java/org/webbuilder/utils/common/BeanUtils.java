b2967f8c7a3cfcb9e5e73332806a7c06fe1ac674
周浩 <zh.sqy@qq.com>
2016-04-06 10:46:16 +0800

    优化性能，新增功能

--- wb-utils/src/main/java/org/webbuilder/utils/common/BeanUtils.java
+++ wb-utils/src/main/java/org/webbuilder/utils/common/BeanUtils.java
@@ -2,7 +2,9 @@ package org.webbuilder.utils.common;
 
 import org.webbuilder.utils.common.bean.AttributeUtil;
 import org.webbuilder.utils.common.bean.JDKAttributeUtil;
+import org.webbuilder.utils.common.bean.imp.CommonAttributeUtil;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,28 +16,25 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by 浩 on 2015-12-09 0009.
  */
 public class BeanUtils {
-
-    private static final AttributeUtil jdkAttributeUtil = new JDKAttributeUtil();
-
+    private static final AttributeUtil attributeUtil = CommonAttributeUtil.getInstance();
 
     public static final <T> T attr(String attr, Object value) {
         try {
-            return jdkAttributeUtil.attr(attr, value);
+            return attributeUtil.attr(attr, value);
         } catch (Exception e) {
             return null;
         }
     }
 
     public static final void attr(Object object, String attr, Object value) throws Exception {
-        jdkAttributeUtil.attr(object, attr, value);
+        attributeUtil.attr(object, attr, value);
     }
-
 
     private static final Map<Class, List<Method>> getterCache = new ConcurrentHashMap<>();
 
     private static final Map<Class, List<Method>> setterCache = new ConcurrentHashMap<>();
 
-    private static List<Method> getGetterFromCache(Class clazz) {
+    public static List<Method> getGetterFromCache(Class clazz) {
         List<Method> methods = getterCache.get(clazz);
         if (methods == null) {
             methods = new ArrayList<>();
@@ -55,7 +54,7 @@ public class BeanUtils {
         return methods;
     }
 
-    private static List<Method> getSetterFromCache(Class clazz) {
+    public static List<Method> getSetterFromCache(Class clazz) {
         List<Method> methods = setterCache.get(clazz);
         if (methods == null) {
             methods = new ArrayList<>();
@@ -82,8 +81,15 @@ public class BeanUtils {
             byte flag = 3;
             if (methodName.startsWith("is")) {
                 flag = 2;
+                Field f = JDKAttributeUtil.getFiledFromCache(source.getClass(), methodName);
+                if (f != null) {
+                    attrName = f.getName();
+                } else {
+                    attrName = String.valueOf(methodName.charAt(flag)).toLowerCase().concat(methodName.substring(flag + 1));
+                }
+            } else {
+                attrName = String.valueOf(methodName.charAt(flag)).toLowerCase().concat(methodName.substring(flag + 1));
             }
-            attrName = String.valueOf(methodName.charAt(flag)).toLowerCase().concat(methodName.substring(flag + 1));
             try {
                 Object data = method.invoke(source);
                 if (data == null && skipNull) continue;
@@ -92,12 +98,6 @@ public class BeanUtils {
             }
         }
         return target;
-    }
-
-    public static void main(String[] args) {
-        String methodName = "getName";
-        String method = String.valueOf(methodName.charAt(3)).toLowerCase().concat(methodName.substring(4));
-        System.out.println(method);
     }
 
 }
--- wb-utils/src/main/java/org/webbuilder/utils/common/ClassUtils.java
+++ wb-utils/src/main/java/org/webbuilder/utils/common/ClassUtils.java
@@ -112,6 +112,39 @@ public class ClassUtils {
         }
     }
 
+    /**
+     * 将对象转为指定的类型
+     * <br/>
+     * 支持日期，数字，boolean类型转换
+     *
+     * @param value 需要转换的值
+     * @param type  目标类型
+     * @return 转换后的值
+     */
+    public static final <T> T cast(Object value, Class<T> type) {
+        if (value == null) return null;
+        Object newVal = null;
+        if (ClassUtils.instanceOf(value.getClass(), type)) {
+            newVal = value;
+        } else if (type == Integer.class || type == int.class) {
+            newVal = StringUtils.toInt(value);
+        } else if (type == Double.class || type == double.class || type == Float.class || type == float.class) {
+            newVal = StringUtils.toDouble(value);
+        } else if (type == Long.class || type == long.class) {
+            newVal = StringUtils.toLong(value);
+        } else if (type == Boolean.class || type == boolean.class) {
+            newVal = StringUtils.isTrue(value);
+        } else if (type == Date.class) {
+            newVal = DateTimeUtils.formatUnknownString2Date(value.toString());
+        } else if (type == String.class) {
+            if (value instanceof Date) {
+                newVal = DateTimeUtils.format(((Date) value), DateTimeUtils.YEAR_MONTH_DAY_HOUR_MINUTE_SECOND);
+            } else
+                newVal = String.valueOf(value);
+        }
+        return (T) newVal;
+    }
+
     public static final Set<Class> commonClass = new HashSet<>();
 
     static {
@@ -121,6 +154,7 @@ public class ClassUtils {
         commonClass.add(byte.class);
         commonClass.add(short.class);
         commonClass.add(char.class);
+        commonClass.add(String.class);
     }
 
     public static boolean isCommonClass(Class clazz) {
